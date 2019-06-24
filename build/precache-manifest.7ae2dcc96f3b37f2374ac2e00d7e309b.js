a669a7f336305493afd3b5cb27f4040a67f0cca8
DESKTOP-U235624\fractal <gamesthatcan@gmail.com>
2019-06-23 23:25:12 -0400

    changed gitignore removed build directory

---
+++ build/precache-manifest.7ae2dcc96f3b37f2374ac2e00d7e309b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd9d2b088ccbc61d4e6fa9211870a35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36d5f9a766d81a51777f",
+    "url": "/static/js/2.87744610.chunk.js"
+  },
+  {
+    "revision": "81e4c5439f4ca2a9dda4",
+    "url": "/static/js/main.072ad9b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
